e08472dfb2ecc941093222ea975ee7d9d961576a
jayseeeeee <dacasin.johnchristian@outlook.com>
2026-04-19 19:44:08 +0800

    feat: seperate table and column manipulation to their separate sql scripts

--- sql/TABLES.sql
+++ sql/TABLES.sql
@@ -20,21 +20,3 @@ RENAME TABLE test TO subject;
 
 -- Deletes the table 'subject'
 DROP TABLE subject;
-
--- Add a new column 'quotation' in the table 'subject'
-ALTER TABLE subject ADD quotation TEXT;
-
--- Rename the column 'quotation' to 'catchphrase' instead
-ALTER TABLE subject RENAME COLUMN quotation TO catchphrase;
-
--- Change the column 'catchphrase' datatype from 'text' to 'varchar' with 500 max characters instead
-ALTER TABLE subject MODIFY COLUMN catchphrase VARCHAR(500);
-
--- Change the column datatype and sequence
--- The 'subject_number' column should come after the 'last_name' column
-ALTER TABLE subject MODIFY subject_number INT AFTER last_name;
--- The 'subject_number' column is placed first before all column
-ALTER TABLE subject MODIFY subject_number INT FIRST;
-
--- Deletes the column 'created_at' from the table 'subject'
-ALTER TABLE subject DROP COLUMN created_at;
