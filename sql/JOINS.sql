93cc5d2b7e9ce097513053c8188ac65519ed80f5
jayseeeeee <dacasin.johnchristian@outlook.com>
2026-06-10 23:12:40 +0800

    feat: update join sql with self join

--- sql/JOINS.sql
+++ sql/JOINS.sql
@@ -8,11 +8,16 @@ ON p.subject_number = s.subject_number; -- Columns that must match
 SELECT p.name, p.role, s.subject_number, s.powers
 FROM party AS p -- Source table (every row will show)
 LEFT JOIN subject AS s -- Joining table (only matching rows will show)
-ON p.subject_number = s.subject_number; -- Columns that must match
+ON p.subject_number = s.subject_number;
 
 -- RIGHT JOIN shows all joining rows and matched original rows
-SELECT p.name, p.role, s.subject_number, s.powers -- Specific columns to show from either table
+SELECT p.name, p.role, s.subject_number, s.powers
 FROM party AS p -- Source table (only matching rows will show)
 RIGHT JOIN subject AS s -- Joining table (everything will show)
-ON p.subject_number = s.subject_number; -- Columns that must match
+ON p.subject_number = s.subject_number;
 
+-- SELF JOIN joins a copy of table to itself (to compare rows of same table)
+SELECT p.name, p.role
+FROM party AS p -- Source table
+INNER JOIN party AS dp -- Joins source and its copy and shows everything from source table
+ON p.subject_number = dp.party_id; -- Shows values that exists on source and copy
